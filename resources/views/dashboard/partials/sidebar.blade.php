3c9ea8cfa2e3ea742e2c1d389786532c48b64fb3
dario208 <dario22.aps2b@gmail.com>
2023-08-05 07:55:45 +0300

    change privilege

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -99,11 +99,8 @@
                     </li>
                 </ul>
             </li>
-        @elseif (Auth::check() && Auth::user()->type === 'admin')
-            <li class="menu-header small text-uppercase">
-                <span class="menu-header-text">Pages</span>
-            </li>
-        @elseif (Auth::check() && Auth::user()->type === 'admin')
+        
+        @elseif (Auth::check() && Auth::user()->type === 'prof')
             <li class="menu-item">
                 <a href="javascript:void(0);" class="menu-link menu-toggle">
                     <i class="menu-icon tf-icons bx bx-dock-top"></i>
