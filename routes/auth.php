08c55dd4b9da7117d50c426780224baa8734a55c
zandryArchiTech <donjuan@esti.mg>
2023-08-05 14:16:34 +0300

    resolution conflit

--- routes/auth.php
+++ routes/auth.php
@@ -14,7 +14,6 @@ Route::middleware('guest')->group(function () {
         Route::post('login', 'store');
     });
 });
-
 Route::middleware('auth')->group(function () {
 
     Route::controller(registeredUserController::class)->group(function () {
