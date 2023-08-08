926e9532cd3240ff3db438585ed4da3f9c0f49ba
zandryArchiTech <donjuan@esti.mg>
2023-08-08 21:32:23 +0300

    historiqueController avance

--- app/Models/Historique.php
+++ app/Models/Historique.php
@@ -11,8 +11,31 @@ class Historique extends Model
 
     protected $fillable =['date','heure_de_debut','heure_de_fin','total_heure','module_id'];
 
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($historique) {
+            // Convertir les heures de début et de fin en objets Carbon pour effectuer le calcul
+            $heureDeDebut = Carbon::parse($historique->heure_de_debut);
+            $heureDeFin = Carbon::parse($historique->heure_de_fin);
+
+            // Calculer la différence entre les heures de fin et de début
+            $differenceHeures = $heureDeFin->diffInHours($heureDeDebut);
+
+            // Stocker le résultat dans la colonne total_heure
+            $historique->total_heure = $differenceHeures;
+        });
+    }
+
+    
+
+
     public function module(){
 
         return $this->belongsTo(Module::class);
     }
+
+   
 }
