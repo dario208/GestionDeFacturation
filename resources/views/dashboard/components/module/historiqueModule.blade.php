4205123f7b42de882841621a1841beedb26426e4
CousemaAnjary <anjaryabdillah@gmail.com>
2023-08-10 09:57:33 +0300

    ajout

--- resources/views/dashboard/components/module/historiqueModule.blade.php
+++ resources/views/dashboard/components/module/historiqueModule.blade.php
@@ -24,19 +24,17 @@
                                 <td>{{ $historique->heure_de_fin }}</td>
                                 <td>{{ $historique->total_heure }}</td>
                             </tr>
-
-                            {{-- <tr>
-    
-                                <td colspan="3"class="borderless"></td>
-                                <td><span>data</span>/<span>data</span></td>
-                            </tr> --}}
                         @endforeach
+                        <tr>
+                            <td colspan="3"class="borderless"></td>
+                            <td>Tolal: <span>{{$heure_effectue}} </span>heures sur <span>{{$heure_globale}}</span></td>
+                        </tr>
                     </tbody>
                 </table>
+
+                soit: {{$montant_total}} Ariary
             </div>
 
- 
-                   
 
         </div>
 
