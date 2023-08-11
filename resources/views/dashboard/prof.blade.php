489076060a0e188d7976a0d2c7f58b93cdafca30
dario208 <dario22.aps2b@gmail.com>
2023-08-11 11:59:13 +0300

    prfs

--- resources/views/dashboard/prof.blade.php
+++ resources/views/dashboard/prof.blade.php
@@ -144,7 +144,7 @@
                     <div class="card">
                         <div class="row row-bordered g-0">
                             <div class="col-md-8">
-                                <h5 class="card-header m-0 me-2 pb-3">Total de Rémuneration</h5>
+                                <h5 class="card-header m-0 me-2 pb-3">Statistique de facture</h5>
                                 <div id="totalRevenueChart" class="px-2"></div>
                             </div>
                             <div class="col-md-4">
