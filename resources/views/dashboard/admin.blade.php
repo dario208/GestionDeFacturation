1397ca448c36ba0bd99ed653eb53fd0956887c50
dario208 <dario22.aps2b@gmail.com>
2023-08-12 09:22:24 +0300

    change UI/UX

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -196,26 +196,16 @@
                     <div class="card h-100">
                         <div class="card-header d-flex align-items-center justify-content-between pb-0">
                             <div class="card-title mb-0">
-                                <h5 class="m-0 me-2">Order Statistics</h5>
-                                <small class="text-muted">42.82k Total Sales</small>
+                                <h5 class="m-0 me-2">Statistique d'étudiant</h5>
+                                
                             </div>
-                            <div class="dropdown">
-                                <button class="btn p-0" type="button" id="orederStatistics" data-bs-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="orederStatistics">
-                                    <a class="dropdown-item" href="javascript:void(0);">Select All</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Refresh</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Share</a>
-                                </div>
-                            </div>
+                            
                         </div>
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <div class="d-flex flex-column align-items-center gap-1">
-                                    <h2 class="mb-2">8,258</h2>
-                                    <span>Total Orders</span>
+                                    <h2 class="mb-2">500</h2>
+                                    <span>étudiants</span>
                                 </div>
                                 <div id="orderStatisticsChart"></div>
                             </div>
@@ -227,11 +217,11 @@
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <h6 class="mb-0">Electronic</h6>
-                                            <small class="text-muted">Mobile, Earbuds, TV</small>
+                                            <h6 class="mb-0">Reussite</h6>
+                                            
                                         </div>
                                         <div class="user-progress">
-                                            <small class="fw-semibold">82.5k</small>
+                                            <small class="fw-semibold">480</small>
                                         </div>
                                     </div>
                                 </li>
@@ -242,11 +232,11 @@
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <h6 class="mb-0">Fashion</h6>
-                                            <small class="text-muted">T-shirt, Jeans, Shoes</small>
+                                            <h6 class="mb-0">Explusion</h6>
+                                            
                                         </div>
                                         <div class="user-progress">
-                                            <small class="fw-semibold">23.8k</small>
+                                            <small class="fw-semibold">5</small>
                                         </div>
                                     </div>
                                 </li>
@@ -257,29 +247,15 @@
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <h6 class="mb-0">Decor</h6>
-                                            <small class="text-muted">Fine Art, Dining</small>
+                                            <h6 class="mb-0">Redoublant</h6>
+                                            
                                         </div>
                                         <div class="user-progress">
-                                            <small class="fw-semibold">849k</small>
+                                            <small class="fw-semibold">15</small>
                                         </div>
                                     </div>
                                 </li>
-                                <li class="d-flex">
-                                    <div class="avatar flex-shrink-0 me-3">
-                                        <span class="avatar-initial rounded bg-label-secondary"><i
-                                                class="bx bx-football"></i></span>
-                                    </div>
-                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                        <div class="me-2">
-                                            <h6 class="mb-0">Sports</h6>
-                                            <small class="text-muted">Football, Cricket Kit</small>
-                                        </div>
-                                        <div class="user-progress">
-                                            <small class="fw-semibold">99</small>
-                                        </div>
-                                    </div>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
@@ -290,50 +266,21 @@
                 <div class="col-md-6 col-lg-4 order-1 mb-4">
                     <div class="card h-100">
                         <div class="card-header">
-                            <ul class="nav nav-pills" role="tablist">
-                                <li class="nav-item">
-                                    <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
-                                        data-bs-target="#navs-tabs-line-card-income"
-                                        aria-controls="navs-tabs-line-card-income" aria-selected="true">
-                                        Income
-                                    </button>
-                                </li>
-                                <li class="nav-item">
-                                    <button type="button" class="nav-link" role="tab">Expenses</button>
-                                </li>
-                                <li class="nav-item">
-                                    <button type="button" class="nav-link" role="tab">Profit</button>
-                                </li>
-                            </ul>
+                            <div class="card-title mb-0">
+                                <h5 class="m-0 me-2">Statistique des professeurs</h5>
+                                <br>
+                                <div class="d-flex flex-column align-items-center gap-1">
+                                    <h2 class="mb-2">150 </h2>
+                                    <span>professeurs</span>
+                                </div>
+                            </div>
                         </div>
                         <div class="card-body px-0">
                             <div class="tab-content p-0">
                                 <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
-                                    <div class="d-flex p-4 pt-3">
-                                        <div class="avatar flex-shrink-0 me-3">
-                                            <img src="../assets/img/icons/unicons/wallet.png" alt="User" />
-                                        </div>
-                                        <div>
-                                            <small class="text-muted d-block">Total Balance</small>
-                                            <div class="d-flex align-items-center">
-                                                <h6 class="mb-0 me-1">$459.10</h6>
-                                                <small class="text-success fw-semibold">
-                                                    <i class="bx bx-chevron-up"></i>
-                                                    42.9%
-                                                </small>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    
                                     <div id="incomeChart"></div>
-                                    <div class="d-flex justify-content-center pt-4 gap-2">
-                                        <div class="flex-shrink-0">
-                                            <div id="expensesOfWeek"></div>
-                                        </div>
-                                        <div>
-                                            <p class="mb-n1 mt-1">Expenses This Week</p>
-                                            <small class="text-muted">$39 less than last week</small>
-                                        </div>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -346,50 +293,60 @@
                     <div class="card h-100">
                         <div class="card-header d-flex align-items-center justify-content-between">
                             <h5 class="card-title m-0 me-2">Transactions</h5>
-                            <div class="dropdown">
-                                <button class="btn p-0" type="button" id="transactionID" data-bs-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="transactionID">
-                                    <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-                                </div>
-                            </div>
+                            
                         </div>
                         <div class="card-body">
                             <ul class="p-0 m-0">
                                 <li class="d-flex mb-4 pb-1">
                                     <div class="avatar flex-shrink-0 me-3">
-                                        <img src="../assets/img/icons/unicons/paypal.png" alt="User"
+                                        <img src={{"images/bni.png"}} alt="User"
                                             class="rounded" />
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <small class="text-muted d-block mb-1">Paypal</small>
-                                            <h6 class="mb-0">Send money</h6>
+                                            <small class="text-muted d-block mb-1">BNI Madagascar</small>
+                                            <h6 class="mb-0">Transactions</h6>
                                         </div>
-                                        <div class="user-progress d-flex align-items-center gap-1">
-                                            <h6 class="mb-0">+82.6</h6>
-                                            <span class="text-muted">USD</span>
-                                        </div>
+                                        
                                     </div>
                                 </li>
                                 <li class="d-flex mb-4 pb-1">
                                     <div class="avatar flex-shrink-0 me-3">
-                                        <img src="../assets/img/icons/unicons/wallet.png" alt="User"
+                                        <img src={{"images/bfv.png"}} alt="User"
                                             class="rounded" />
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <small class="text-muted d-block mb-1">Wallet</small>
-                                            <h6 class="mb-0">Mac'D</h6>
+                                            <small class="text-muted d-block mb-1">Societé Generale</small>
+                                            <h6 class="mb-0">Transactions</h6>
                                         </div>
-                                        <div class="user-progress d-flex align-items-center gap-1">
-                                            <h6 class="mb-0">+270.69</h6>
-                                            <span class="text-muted">USD</span>
+                                        
+                                    </div>
+                                </li>
+                                <li class="d-flex mb-4 pb-1">
+                                    <div class="avatar flex-shrink-0 me-3">
+                                        <img src={{"images/boa.png"}} alt="User"
+                                            class="rounded" />
+                                    </div>
+                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                        <div class="me-2">
+                                            <small class="text-muted d-block mb-1">BOA</small>
+                                            <h6 class="mb-0">Transactions</h6>
                                         </div>
+                                        
+                                    </div>
+                                </li>
+                                <li class="d-flex mb-4 pb-1">
+                                    <div class="avatar flex-shrink-0 me-3">
+                                        <img src={{"images/baobab.png"}} alt="User"
+                                            class="rounded" />
+                                    </div>
+                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                        <div class="me-2">
+                                            <small class="text-muted d-block mb-1">Baobab</small>
+                                            <h6 class="mb-0">Transactions</h6>
+                                        </div>
+                                        
                                     </div>
                                 </li>
 
@@ -398,7 +355,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
--- resources/views/dashboard/comptable.blade.php
+++ resources/views/dashboard/comptable.blade.php
@@ -208,7 +208,7 @@
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <div class="d-flex flex-column align-items-center gap-1">
-                                    <h2 class="mb-2">500 </h2>
+                                    <h2 class="mb-2">500</h2>
                                     <span>étudiants</span>
                                 </div>
                                 <div id="orderStatisticsChart"></div>
@@ -221,11 +221,11 @@
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <h6 class="mb-0">Electronic</h6>
-                                            <small class="text-muted">Mobile, Earbuds, TV</small>
+                                            <h6 class="mb-0">Reussite</h6>
+                                            
                                         </div>
                                         <div class="user-progress">
-                                            <small class="fw-semibold">82.5k</small>
+                                            <small class="fw-semibold">480</small>
                                         </div>
                                     </div>
                                 </li>
@@ -236,11 +236,11 @@
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <h6 class="mb-0">Fashion</h6>
-                                            <small class="text-muted">T-shirt, Jeans, Shoes</small>
+                                            <h6 class="mb-0">Explusion</h6>
+                                            
                                         </div>
                                         <div class="user-progress">
-                                            <small class="fw-semibold">23.8k</small>
+                                            <small class="fw-semibold">5</small>
                                         </div>
                                     </div>
                                 </li>
@@ -251,29 +251,15 @@
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <h6 class="mb-0">Decor</h6>
-                                            <small class="text-muted">Fine Art, Dining</small>
+                                            <h6 class="mb-0">Redoublant</h6>
+                                            
                                         </div>
                                         <div class="user-progress">
-                                            <small class="fw-semibold">849k</small>
+                                            <small class="fw-semibold">15</small>
                                         </div>
                                     </div>
                                 </li>
-                                <li class="d-flex">
-                                    <div class="avatar flex-shrink-0 me-3">
-                                        <span class="avatar-initial rounded bg-label-secondary"><i
-                                                class="bx bx-football"></i></span>
-                                    </div>
-                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                        <div class="me-2">
-                                            <h6 class="mb-0">Sports</h6>
-                                            <small class="text-muted">Football, Cricket Kit</small>
-                                        </div>
-                                        <div class="user-progress">
-                                            <small class="fw-semibold">99</small>
-                                        </div>
-                                    </div>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
@@ -311,50 +297,60 @@
                     <div class="card h-100">
                         <div class="card-header d-flex align-items-center justify-content-between">
                             <h5 class="card-title m-0 me-2">Transactions</h5>
-                            <div class="dropdown">
-                                <button class="btn p-0" type="button" id="transactionID" data-bs-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    <i class="bx bx-dots-vertical-rounded"></i>
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="transactionID">
-                                    <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-                                </div>
-                            </div>
+                            
                         </div>
                         <div class="card-body">
                             <ul class="p-0 m-0">
                                 <li class="d-flex mb-4 pb-1">
                                     <div class="avatar flex-shrink-0 me-3">
-                                        <img src="../assets/img/icons/unicons/paypal.png" alt="User"
+                                        <img src={{"images/bni.png"}} alt="User"
                                             class="rounded" />
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <small class="text-muted d-block mb-1">Paypal</small>
-                                            <h6 class="mb-0">Send money</h6>
+                                            <small class="text-muted d-block mb-1">BNI Madagascar</small>
+                                            <h6 class="mb-0">Transactions</h6>
                                         </div>
-                                        <div class="user-progress d-flex align-items-center gap-1">
-                                            <h6 class="mb-0">+82.6</h6>
-                                            <span class="text-muted">USD</span>
-                                        </div>
+                                        
                                     </div>
                                 </li>
                                 <li class="d-flex mb-4 pb-1">
                                     <div class="avatar flex-shrink-0 me-3">
-                                        <img src="../assets/img/icons/unicons/wallet.png" alt="User"
+                                        <img src={{"images/bfv.png"}} alt="User"
                                             class="rounded" />
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
-                                            <small class="text-muted d-block mb-1">Wallet</small>
-                                            <h6 class="mb-0">Mac'D</h6>
+                                            <small class="text-muted d-block mb-1">Societé Generale</small>
+                                            <h6 class="mb-0">Transactions</h6>
                                         </div>
-                                        <div class="user-progress d-flex align-items-center gap-1">
-                                            <h6 class="mb-0">+270.69</h6>
-                                            <span class="text-muted">USD</span>
+                                        
+                                    </div>
+                                </li>
+                                <li class="d-flex mb-4 pb-1">
+                                    <div class="avatar flex-shrink-0 me-3">
+                                        <img src={{"images/boa.png"}} alt="User"
+                                            class="rounded" />
+                                    </div>
+                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                        <div class="me-2">
+                                            <small class="text-muted d-block mb-1">BOA</small>
+                                            <h6 class="mb-0">Transactions</h6>
                                         </div>
+                                        
+                                    </div>
+                                </li>
+                                <li class="d-flex mb-4 pb-1">
+                                    <div class="avatar flex-shrink-0 me-3">
+                                        <img src={{"images/baobab.png"}} alt="User"
+                                            class="rounded" />
+                                    </div>
+                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                        <div class="me-2">
+                                            <small class="text-muted d-block mb-1">Baobab</small>
+                                            <h6 class="mb-0">Transactions</h6>
+                                        </div>
+                                        
                                     </div>
                                 </li>
 
