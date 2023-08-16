a6d83275d3ad85ec9bf67a9d94543ed827e63cd9
dario208 <dario22.aps2b@gmail.com>
2023-08-16 18:33:27 +0300

    change dash

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -90,7 +90,7 @@
                             </div>
                             <div class="col-sm-5 text-center text-sm-left">
                                 <div class="card-body pb-0 px-0 px-md-4">
-                                    <img src="{{ asset('images/teachers.png') }}" height="160" alt="View Badge User"
+                                    <img src="{{ asset('images/market.png') }}" height="160" alt="View Badge User"
                                         data-app-dark-img="illustrations/man-with-laptop-dark.png"
                                         data-app-light-img="illustrations/man-with-laptop-light.png">
                                         
